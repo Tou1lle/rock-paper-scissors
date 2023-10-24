719d8acc4be9c92298bc2dce1f80dce9de363cca
Tuan Ha <hatuan@email.cz>
2023-10-24 08:28:36 +0200

    complete the playround function - play 1 round of rockpaperscissor

--- js/script.js
+++ js/script.js
@@ -25,13 +25,34 @@ console.log(getComputerChoice());
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
+    // If statement for player's rock 
     if (playerSelection === rock && computerSelection === rock) {
         return "Tie!";
     } else if (playerSelection === rock && computerSelection === paper) {
         return "You Lose! Paper beats Rock";
+    } else if (playerSelection === rock && computerSelection === scissors) {
+        return "You win! Rock beats Scissors";
+    }
+
+    //If statement for player's scissors
+    if (playerSelection === scissors && computerSelection === scissors) {
+        return "Tie!";
+    } else if (playerSelection === scissors && computerSelection === rock) {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection === scissors && computerSelection === paper) {
+        return "You win! Scissors beat Paper";
+    }
+
+    //If statement for player's paper
+    if (playerSelection === paper && computerSelection === paper) {
+        return "Tie!";
+    } else if (playerSelection === paper && computerSelection === scissors) {
+        return "You Lose! Scissors beat Paper";
+    } else if (playerSelection === paper && computerSelection === rock) {
+        return "You win! Paper beats Rock";
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = "Paper";
+const playerSelection = "sCiSSors";
+const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
