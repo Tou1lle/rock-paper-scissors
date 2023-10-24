fd618072ce48b51b89b453de23bb0b00cf735804
Tuan Ha <hatuan@email.cz>
2023-10-24 08:21:07 +0200

    complete the prototype of the game

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 console.log("hello world! Touille's here");
+
+/*
+Make a computer choose between rock, paper and scissors
+
+Player chooses between rock paper scissors
+
+Verdict - win or lose
+*/
+
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+function getComputerChoice() {
+    const choices = [rock, paper, scissors];
+
+    const index = Math.floor(Math.random() * choices.length);
+
+    return choices[index];
+}
+
+console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    if (playerSelection === rock && computerSelection === rock) {
+        return "Tie!";
+    } else if (playerSelection === rock && computerSelection === paper) {
+        return "You Lose! Paper beats Rock";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = "Paper";
+console.log(playRound(playerSelection, computerSelection));
